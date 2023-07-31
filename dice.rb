5ebd088a5e88348c6a1018f9de1cb8d9017c8d71
J-Me-Z <136928731+J-Me-Z@users.noreply.github.com>
2023-07-31 00:47:12 +0000

    main

--- dice.rb
+++ dice.rb
@@ -9,8 +9,12 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+erb(:elephant)
 end
+
+#get("/") do
+ # "Hello World"
+#end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
@@ -25,8 +29,47 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six, { :layout => :wrapper})
+	
+ # "<h1>2d6</h1>
+  # <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+ # second_die = rand(1..20)
+  sum = first_die #+ second_die
+	
+  outcome = "You rolled a #{first_die} ."
+ # and a {second_die} for a total of #{sum}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die}  and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
    <p>#{outcome}</p>"
 end
