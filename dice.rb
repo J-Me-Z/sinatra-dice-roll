ef559a46480eb335032113c42b5c43e50010d009
J-Me-Z <136928731+J-Me-Z@users.noreply.github.com>
2023-07-31 01:21:10 +0000

    testing

--- dice.rb
+++ dice.rb
@@ -28,12 +28,11 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
-  erb(:two_six, { :layout => :wrapper})
 	
- "<h1>2d6</h1>
- <p>#{outcome}</p>"
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
 
 get("/dice/2/10") do
