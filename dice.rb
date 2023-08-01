81ccd7f58d34790e1c8b70a45d134d5fe4692aec
J-Me-Z <136928731+J-Me-Z@users.noreply.github.com>
2023-08-01 02:50:51 +0000

    almost done

--- dice.rb
+++ dice.rb
@@ -50,11 +50,11 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @die= rand(1..20)
  # second_die = rand(1..20)
-  sum = first_die #+ second_die
+  
 	
-  @outcome = "You rolled a #{first_die}."
+  @outcome = "You rolled a #{@die}."
  # and a {second_die} for a total of #{sum}."
 
  erb(:one_twenty, {:layout => :wrapper})
@@ -78,4 +78,16 @@ get("/dice/5/4") do
 	
   # "<h1>5d4</h1>
   #  <p>#{outcome}</p>"
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
